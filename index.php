345bc9eff58607ea3541262ba014b1a95ba14688
新川 <ARAKAWA@usernoiMac-2.local>
2022-01-05 10:46:02 +0900

    2021/1/5(水)/desktop/index画面グリッド再構成一時中断

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- bootstrap4 -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- bootstrap5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="/self_portal_site/css/index.css?ver=<?php echo time(); ?>">
   <title>SELF PORTAL</title>
 </head>
 
 <body>
-  <div class="container">
+  <!-- <div class="container">
     <div class="card mt-2">
       <div class="card-header text-center">Arakawa_Work_Suport</div>
-      <div class="card-body d-flex justify-content-between">
+      <div class="card-body row">
         <div class="card col-2">
           <div class="card-header text-nowrap">MENU</div>
         </div>
@@ -33,7 +33,7 @@
               <div class="form-group row">
                 <label for="home_time" class="col-sm-2 col-form-label">退勤</label>
                 <div class="col-sm-10">
-                  <input class="form-control" type="text" name="home_time" id="home_time" onChange="home_time()">
+                  <input class="form-control" type="text" name="home_time" id="home_time" onChange="homeTime()">
                 </div>
               </div>
             </form>
@@ -44,12 +44,42 @@
         </div>
       </div>
     </div>
+  </div> -->
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-2">
+        <div class="card">
+          <div class="card-header">MENU</div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+      <div class="col-md-7">
+        <div class="card">
+          <div class="card-header">日報</div>
+          <div class="card-body">
+            <div class="row">
+              <label for="work_time" class="col-md-6">出勤</label>
+              <div class="cal-md-6"><input type="text" name="work_time" id="work_time"></div>
+            </div>
+            <div class="row">
+              <label for="home_time" class="col-md-6">退勤</label>
+              <div class="cal-md-6"><input type="text" name="home_time" id="home_time"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-header">NOTE</div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+    </div>
   </div>
 
-  <!-- jquery + bootstrap4 -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <!-- jquery + bootstrap5 -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <!-- jsファイル -->
   <script src="/self_portal_site/js/index.js?ver=<?php echo time(); ?>"></script>
 </body>
