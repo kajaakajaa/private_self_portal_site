eaf4e354504405cab9206e1d81ebbc9044dca07c
新川 <ARAKAWA@usernoiMac-2.local>
2022-01-21 21:07:42 +0900

    2021/1/21(金)/desktop/翌日ボタン解決から

--- js/index.js
+++ js/index.js
@@ -2,6 +2,15 @@ $(()=> {
   setListShift();
 });
 
+//formデータ
+function formDataTemplate() {
+  let query = {};
+  let dateVal = $('#datepicker').val();
+  query['work_date'] = dateVal;
+  query['user_no'] = $('#user_no').val();
+  return query;
+}
+
 //オンロード時に取得するデフォルト値
 function setListShift() {
   let query = {};
@@ -19,6 +28,8 @@ function setListShift() {
         $('#work_time').val(value.work_time);
         $('#home_time').val(value.home_time);
       })
+      let today = data.date;
+      $('#datepicker').datepicker().datepicker('setDate', today);
     },
     function(jgHXR, textStatus, errorThrown) {
       console.log(jgHXR);
@@ -67,6 +78,76 @@ function homeTime() {
     function(data) {
       console.log(data);
       setListShift();
+    },
+    function(jgXHR, textStatus, errorThrown) {
+      console.log(jgXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
+  );
+}
+
+//日付別でデータを取得
+function getData() {
+  const query = formDataTemplate();
+  $.ajax({
+    type: 'POST',
+    url: '/self_portal_site/request/sql_data.php?mode=get_data',
+    data: query,
+    dataType: 'json'
+  })
+  .then(
+    function(data) {
+      console.log(data);
+      if(data == '') {
+        $('#work_time').val('');
+        $('#home_time').val('');
+      }
+      else {
+        $.each(data.user, (key, value)=> {
+          $('#work_time').val(value.work_time);
+          $('#home_time').val(value.home_time);
+        });
+      }
+    },
+    function(jgXHR, textStatus, errorThrown) {
+      console.log(jgXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
+  );
+}
+
+function getOtherDay(num) {
+  let query = formDataTemplate();
+      query['other_day'] = num;
+  $.ajax({
+    type: 'POST',
+    url: '/self_portal_site/request/sql_data.php?mode=get_other_day',
+    data: query,
+    dataType: 'json'
+  })
+  .then(
+    function(data) {
+      console.log(data);
+      let workDate = value.work_date;
+          workDate = workDate.replaceAll('-', '/');
+      if(data == null) {
+        console.log('kajaa');
+        $.each(data.user, (key, value)=> {
+          $('#datepicker').val(workDate);
+          $('#work_time').val('');
+          $('#home_time').val('');
+        });
+      }
+      else {
+        console.log('nu?');
+        $.each(data.user, (key, value)=> {
+          $('#datepicker').val(workDate);
+          $('#work_time').val(value.work_time);
+          $('#home_time').val(value.home_time);
+        });
+      }
     },
     function(jgXHR, textStatus, errorThrown) {
       console.log(jgXHR);
--- request/sql_data.php
+++ request/sql_data.php
@@ -1,6 +1,11 @@
 <?php
 include_once('../../config/db_connect.php');
 
+$year = date('Y');
+$month = date('m');
+$day = date('d');
+$timestamp = mktime(0,0,0,$month,$day,$year);
+$date = date('Y/n/j', $timestamp);
 $mode = $_GET['mode'];
 switch($mode) {
   case 'set_list_shift':
@@ -24,9 +29,11 @@ switch($mode) {
 EOF;
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
+    $array = array();
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
       $array['user'][] = $row;
     }
+    $array['date'] = $date;
     header('Content-type: application/json; charset=UTF-8');
     echo json_encode($array);
   break;
@@ -71,5 +78,81 @@ EOF;
     $stmt->bindParam(':user_no', $userNo, PDO::PARAM_INT);
     $stmt->execute();
     var_dump($stmt->errorInfo());
+  break;
+
+  case 'get_data':
+    $workDate = $_POST['work_date'];
+    $userNo = $_POST['user_no'];
+    $sql = <<<EOF
+      SELECT
+        DATE_FORMAT
+        (
+          work_time,
+          '%H:%i'
+        ) AS work_time,
+        DATE_FORMAT
+        (
+          home_time,
+          '%H:%i'
+        ) AS home_time
+      FROM
+        tbl_work_time
+      WHERE
+        work_date = :work_date
+          AND
+        no = :user_no
+          AND
+        del_flg = 0
+EOF;
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':work_date', $workDate, PDO::PARAM_STR);
+    $stmt->bindParam(':user_no', $userNo, PDO::PARAM_INT);
+    $stmt->execute();
+    $array = array();
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      $array['user'][] = $row;
+    }
+    header('Content-type: application/json; charset=UTF-8');
+    echo json_encode($array);
+  break;
+
+  case 'get_other_day':
+    $workDate = $_POST['work_date'];
+    $userNo = $_POST['user_no'];
+    $otherDay = $_POST['other_day'];
+    $day = date('d', strtotime($workDate));
+    $otherDay == 1 ? $timestamp = mktime(0,0,0,$month,$day-1,$year) : $timestamp = mktime(0,0,0,$month,$day+1,$year);
+    $date = date('Y/n/j', $timestamp);
+    $sql = <<<EOF
+      SELECT
+        work_date,
+          DATE_FORMAT
+          (
+            work_time,
+            '%H:%i'
+          ) AS work_time,
+          DATE_FORMAT
+          (
+            home_time,
+            '%H:%i'
+          ) AS home_time
+      FROM
+        tbl_work_time
+      WHERE
+        no = :user_no
+          AND
+        work_date = :date
+          AND
+        del_flg = 0
+EOF;
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':user_no', $userNo, PDO::PARAM_INT);
+    $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+    $stmt->execute();
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      $array['user'][] = $row;
+    }
+    header('Content-type: application/json; charset=UTF-8');
+    echo json_encode($array);
   break;
 }
