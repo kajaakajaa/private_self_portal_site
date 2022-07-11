a6275d526090dba31bf7087063d23d5ae2a9dbd9
kajaakajaa <rwqxh8703@yahoo.co.jp>
2022-07-11 16:15:00 +0900

    2022/7/11(月)/15mac/htmlspecialcahrs_decodeで正しく変換、追加実装

--- request/category_sql_data.php
+++ request/category_sql_data.php
@@ -6,6 +6,9 @@ $mode = $_GET['mode'];
 function h($str) {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+function hd($str) {
+  return htmlspecialchars_decode($str, ENT_QUOTES);
+}
 
 switch($mode) {
   case 'set_list_category':
@@ -51,8 +54,10 @@ EOF;
     $stmt->execute();
     $array['category'] = $stmt->fetch(PDO::FETCH_ASSOC);
     $array['contents_nobr'] = $array['category']['contents'];
-    $array['contents_nobr'] = h($array['contents_nobr']);
+    $array['contents_nobr'] = $array['contents_nobr'];
     $array['contents_nobr'] = nl2br($array['contents_nobr'], false);
+    //編集コンテンツをhtmlspecialchars_decodeで復元
+    $array['category']['contents'] = hd($array['category']['contents']);
     header('Content-type: application/json; charset=UTF-8');
     echo json_encode($array);
     exit;
@@ -84,7 +89,7 @@ EOF;
     $stmt = $dbh->prepare($sql);
     $stmt->bindParam(':user_no', $userNo, PDO::PARAM_STR);
     $stmt->bindParam(':menu_no', $menuNo, PDO::PARAM_STR);
-    $stmt->bindParam(':contents', $contents, PDO::PARAM_STR);
+    $stmt->bindParam(':contents', h($contents), PDO::PARAM_STR);
     $stmt->execute();
     var_dump($stmt->errorInfo());
   break;
